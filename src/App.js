661816f2462ae7a9e61f89cad8ee91b7857de05b
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-04 13:37:08 +0500

    fixed some of imports bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import { useFetch } from './Hooks/useFetch'
+import Follower from './Components/Follower'
 function App() {
   return <h2>pagination starter</h2>
 }
