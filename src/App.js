1e1e358ede9062343ff0f9e5b5b04b63675b9708
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-04 13:56:42 +0500

    workin on the pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './Hooks/useFetch'
-import Follower from './Components/Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./Hooks/useFetch";
+import Follower from "./Components/Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className="underline" />
+      </div>
+      <div className='container'>
+          {data.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
-const paginate = () => {}
+const paginate = (paginate) => {
+let items_per_page=5
+let PageItems= Math.ceil(paginate.length/items_per_page);
+    console.log(PageItems)
+}
 
 export default paginate
