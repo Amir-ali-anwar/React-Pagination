9840207d73d232ab3f56041e01c4abba3dee0ce8
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-04 16:20:54 +0500

    pagination completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import Follower from "./Components/Follower";
 function App() {
   const { loading, data } = useFetch();
   const [follower, Setfollowers] = useState([]);
-  const [page, Setpage] = useState(1);
+  const [page, Setpage] = useState(0);
   //  console.log(follower);
   useEffect(() => {
     if (loading) return;
     Setfollowers(data[page]);
   }, [loading, page]);
+  const prevpage = () => {
+    Setpage((oldpage) => {
+      let nextPage = oldpage - 1;
+      if (nextPage > data.length - 1|| nextPage <0) {
+        nextPage = data.length - 1;
+      }
+      return nextPage;
+    });
+  };
+  const nextpage = () => {
+    Setpage((oldpage) => {
+      let nextPage = oldpage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
   return (
     <main>
       <div className="section-title">
@@ -24,19 +42,23 @@ function App() {
         </div>
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevpage}>
+              prev
+            </button>
             {data?.map((item, index) => {
               return (
                 <button
                   key={index}
-                  className={`page-btn ${
-                    index === page ? "active-btn" : ""
-                  }`}
+                  className={`page-btn ${index === page ? "active-btn" : ""}`}
                   onClick={() => Setpage(index)}
                 >
                   {index + 1}
                 </button>
               );
             })}
+            <button className="next-btn" onClick={nextpage}>
+              next
+            </button>
           </div>
         )}
       </section>
