d8d33a596ac06ffebc91453946096e54c72b29de
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-04 16:04:22 +0500

    working on the pagination

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import { useFetch } from "./Hooks/useFetch";
 import Follower from "./Components/Follower";
 function App() {
   const { loading, data } = useFetch();
-  const [follower,Setfollowers]=useState([])
-  const [page,Setpage]=useState(1)
-//  console.log(follower);
+  const [follower, Setfollowers] = useState([]);
+  const [page, Setpage] = useState(1);
+  //  console.log(follower);
   useEffect(() => {
-    if(loading) return
-    Setfollowers(data[page])
-  }, [loading]);
+    if (loading) return;
+    Setfollowers(data[page]);
+  }, [loading, page]);
   return (
     <main>
       <div className="section-title">
-        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <h1>{loading ? "loading..." : "pagination"}</h1>
         <div className="underline" />
       </div>
-      <div className='container'>
+      <section className="followers">
+        <div className="container">
           {follower?.map((follower) => {
-            return <Follower key={follower.id} {...follower} />
+            return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+            {data?.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${
+                    index === page ? "active-btn" : ""
+                  }`}
+                  onClick={() => Setpage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </section>
     </main>
   );
 }
