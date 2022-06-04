b2f561a10334ef9d00ce9862c289fdc23d954f21
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-04 14:17:32 +0500

    working on pagination

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
 const paginate = (paginate) => {
 let items_per_page=5
 let PageItems= Math.ceil(paginate.length/items_per_page);
-    console.log(PageItems)
+const Newfollowers= Array.from({length:PageItems},(_,index)=>{
+    let start= index*items_per_page
+    return paginate.slice(start,start+items_per_page)
+})
+console.log(Newfollowers);
 }
 
 export default paginate
