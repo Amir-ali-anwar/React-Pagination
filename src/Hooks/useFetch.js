2fcc7f90fff1de716000304f9a79d86f2817d25d
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-27 22:37:23 +0500

    little fix

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import paginate from '../utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=200'
+const url = 'https://api.github.com/users/bradtraversy/followers?per_page=200'
 
 export const useFetch = () => {
   const [loading, setLoading] = useState(true)
