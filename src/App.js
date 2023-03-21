bb99e239f885dee68c2a1bdf1425258f8563972b
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-21 20:55:12 +0500

    added loader component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useFetch } from "./Hooks/useFetch";
 import Follower from "./Components/Follower";
+import Loader from "./Components/Loader";
 function App() {
   const { loading, data } = useFetch();
   const [follower, Setfollowers] = useState([]);
@@ -13,7 +14,7 @@ function App() {
   const prevpage = () => {
     Setpage((oldpage) => {
       let nextPage = oldpage - 1;
-      if (nextPage > data.length - 1|| nextPage <0) {
+      if (nextPage > data.length - 1 || nextPage < 0) {
         nextPage = data.length - 1;
       }
       return nextPage;
@@ -31,7 +32,7 @@ function App() {
   return (
     <main>
       <div className="section-title">
-        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <h1>{loading ? <Loader /> : "pagination"}</h1>
         <div className="underline" />
       </div>
       <section className="followers">
